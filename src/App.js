504e3b0999f41e60c451a9fbd0f8dd86edcd97d7
Victor Tran <victor.n.tran@outlook.com>
2019-11-11 09:22:39 -0800

    pass cart state into CartContext value prop

--- src/App.js
+++ src/App.js
@@ -20,19 +20,17 @@ function App() {
   };
 
   return (
-    <ProductContext.Provider value={{products, addItem}}>
-      <div className="App">
-        <Navigation cart={cart} />
+    <ProductContext.Provider value={{ products, addItem }}>
+      <CartContext.Provider value={{cart}}>
+        <div className="App">
+          <Navigation cart={cart} />
 
-        {/* Routes */}
-        <Route
-          exact
-          path="/"
-          component={Products}
-        />
+          {/* Routes */}
+          <Route exact path="/" component={Products} />
 
-        <Route path="/cart" component={ShoppingCart} />
-      </div>
+          <Route path="/cart" component={ShoppingCart} />
+        </div>
+      </CartContext.Provider>
     </ProductContext.Provider>
   );
 }
